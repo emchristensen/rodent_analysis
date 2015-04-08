9c378f6c1c5375547da7c37bf505de62285252dd
emchristensen <erica.christensen@weecology.org>
2015-04-08 15:48:20 -0600

    simplified script to only produce time series plot

--- individual_species_ts.r
+++ individual_species_ts.r
@@ -1,23 +1,26 @@
-source('portal_weather/csv_to_dataframe.r')
+# This script is intended to plot species abundance through time for a single species
 
-dat = read.csv("data/Rodents.csv", as.is = TRUE,  colClasses = c(note1='character'))
-# There's a real species called NA, so make sure that the NAs are actually "NA"
-dat$species[is.na(dat$species)] = "NA"
 
-sp = c("PM")
+source('rodent_analysis/get_rodent_data.r')
+
+dat = get_rodent_data('controls',seq(1977,2014))
+
+dat = dat[dat$period>0,]
+
+period_dates = read.csv('data/Period_dates_single.csv')
+
+#========================================================================================
+# aggregate by period
+
+sp = c("DM")
 
 species = dat[dat$species %in% sp,]
 species_count = aggregate(species$yr,
-                          by=list(yr=species$yr),
+                          by=list(period=species$period),
                           FUN=length)
 
+species_count = merge(species_count,period_dates,by='period')
+species_count$date = as.Date(paste(species_count$yr,species_count$mo,species_count$dy,sep='-'))
 
-per1 = aggregate(dat$period,by=list(yr= dat$yr,period=dat$period),
-                    FUN=length)
-periods = aggregate(per1$yr,
-                     by=list(yr=per1$yr),
-                     FUN=length)
-
-species_count = merge(species_count,periods,by.x='yr',by.y='yr')
-plot(species_count$yr,species_count$x.x/species_count$x.y,xlab = '',ylab='animals per census',main=sp)
-lines(species_count$yr,species_count$x.x/species_count$x.y)
+plot(species_count$date,species_count$x,xlab = '',ylab='animals per census',main=sp)
+lines(species_count$date,species_count$x)
