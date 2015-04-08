65efa46093fc04b520f03631eb3351400b899ff7
emchristensen <erica.christensen@weecology.org>
2015-04-08 14:02:17 -0600

    script to plot number of species at Portal through time

--- diversity_per_census.r
+++ diversity_per_census.r
@@ -1,30 +1,53 @@
-source('portal_weather/csv_to_dataframe.r')
+# This script plots species diversity over time
 
+# =============================================================================
+# read in rodent data
 dat = read.csv("data/Rodents.csv", as.is = TRUE,  colClasses = c(note1='character'))
+
 # There's a real species called NA, so make sure that the NAs are actually "NA"
 dat$species[is.na(dat$species)] = "NA"
 
 dat = dat[dat$period>0,]
 
+# only rodent species, no unknowns
+allspecies = c('DM','DS','NA','PE','PF','PP','DO','OL','OT','RM','PM','SH','PH','RF','SF','BA','SO','PI','PB','PL','RO')
+dat = dat[dat$species %in% allspecies,]
+
+periodinfo = read.csv('data/Period_dates_single.csv')
+periodinfo$date = as.Date(paste(periodinfo$yr,periodinfo$mo,periodinfo$dy,sep='-'))
+
+# =============================================================================
+# find  number of species per period
+
 species_count = aggregate(dat$yr,
-                          by=list(yr = dat$yr,period=dat$period,species=dat$species),                          
+                          by=list(period=dat$period,species=dat$species),                          
                           FUN=length)
 
-sp_per_period = aggregate(species_count$x,
-                          by=list(period=species_count$period,yr=species_count$yr),
+sp_per_period = aggregate(species_count$species,
+                          by=list(period=species_count$period),
                           FUN=length)
 
-sp_per_year = aggregate(species_count$x,
-                        by=list(yr=species_count$yr),
-                        FUN=length)
+# ===========================================================================
+# find number of plots trapped per period
 
+trapped = dat[!dat$note1 %in% c(4,8),]
+count_per_plot = aggregate(trapped$plot,
+                           by=list(period=trapped$period,plot=trapped$plot),
+                           FUN=length)
 
-per1 = aggregate(dat$period,by=list(yr= dat$yr,period=dat$period),
-                 FUN=length)
-periods = aggregate(per1$yr,
-                    by=list(yr=per1$yr),
-                    FUN=length)
+plot_per_period = aggregate(count_per_plot$plot,
+                            by=list(period=count_per_plot$period),
+                            FUN=length)
 
-species_count = merge(species_count,periods,by.x='yr',by.y='yr')
-plot(species_count$yr,species_count$x.x/species_count$x.y,xlab = '',ylab='animals per census',main=sp)
-lines(species_count$yr,species_count$x.x/species_count$x.y)
+# ==============================================================================
+# normalize species diversity per period by number of plots trapped each period
+
+sp_diversity = merge(sp_per_period,plot_per_period,by='period')
+names(sp_diversity) = c('period','species','plots')
+sp_diversity$sp_plot = sp_diversity$species/sp_diversity$plots
+sp_diversity = merge(sp_diversity,periodinfo,by='period')
+
+# ============================================================================
+# plot results
+
+plot(sp_diversity$date,sp_diversity$species,ylab='# species',xlab='',main='Species Diversity through time')
